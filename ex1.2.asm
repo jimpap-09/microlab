50b4041108e492ec9999937b89bac757044fe908
Dimitris Papadimitriou <61821123+jimpap-09@users.noreply.github.com>
2024-10-12 22:42:15 +0300

    Update ex1.2.asm

--- ex1.2.asm
+++ ex1.2.asm
@@ -18,12 +18,12 @@ logicF0:
     push C
     push D
 
-    com A		    ; A <- A'
-    com D		    ; D <- D'
-    and A, D		    ; A <- A'D'
-    or B, A		    ; B <- B + A'D'
+    com A		            ; A <- A'
+    com D		            ; D <- D'
+    and A, D		        ; A <- A'D'
+    or B, A		            ; B <- B + A'D'
 
-    mov F0, B		    ; F0 <- B + A'D'
+    mov F0, B		        ; F0 <- B + A'D'
 
     pop D
     pop C
@@ -39,13 +39,13 @@ logicF1:
     push C
     push D
 
-    com C		    ; C <- C'
-    or A, C		    ; A <- A + C'
-    com D		    ; D <- D'
-    or B, D		    ; B <- B + D'
-    and A, B		    ; A <- (A + C')(B + D')
+    com C		            ; C <- C'
+    or A, C		            ; A <- A + C'
+    com D		            ; D <- D'
+    or B, D		            ; B <- B + D'
+    and A, B		        ; A <- (A + C')(B + D')
 
-    mov F1, A		    ; F1 <- (A + C')(B + D')
+    mov F1, A		        ; F1 <- (A + C')(B + D')
 
     pop D
     pop C
@@ -57,9 +57,9 @@ logicF1:
 main:
 ; initialize stack pointer
 ldi r24, LOW(RAMEND)        ; 1 cycle
-out SPL, r24		    ; 2 cycles
+out SPL, r24		        ; 2 cycles
 ldi r24, HIGH(RAMEND)       ; 1 cycle
-out SPH, r24		    ; 2 cycles
+out SPH, r24		        ; 2 cycles
 
 ; initialize reg A, B, C, D
 ldi A, 0x51
@@ -68,21 +68,21 @@ ldi C, 0x21
 ldi D, 0x01
 
 ; initialize the counter
-ldi I, 0x06		    ; Load loop counter
+ldi I, 0x06		            ; Load loop counter
 loop:
-    rcall logicF0	    ; call F0
-    rcall logicF1	    ; call F1
+    rcall logicF0	        ; call F0
+    rcall logicF1	        ; call F1
 
-    inc A		    ; A += 1
+    inc A		            ; A += 1
 
-    ldi T, 0x02		    ; T = temporary register
-    add B, T		    ; B += 2
+    ldi T, 0x02		        ; T = temporary register
+    add B, T		        ; B += 2
 
     ldi T, 0x03
-    add C, T		    ; C += 3
+    add C, T		        ; C += 3
 
     ldi T, 0x04
-    add D, T		    ; D += 4
+    add D, T		        ; D += 4
 
-    dec I		    ; I -= 1
+    dec I		            ; I -= 1
     brne loop
