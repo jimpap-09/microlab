11ff35fd53059920bda47c7c360971efd25c18a4
Dimitris Papadimitriou <61821123+jimpap-09@users.noreply.github.com>
2024-10-31 18:19:34 +0200

    Update ex3.3.c

--- ex3.3.c
+++ ex3.3.c
@@ -1,78 +1,70 @@
-#include <avr/io.h>
-#include <util/delay.h>
-
-// CPU, PWM frequency
 #define F_CPU 16000000UL
 #define F_PWM 62500
 
-int DC_VALUE;
-int OCR1A_table[14] = {5, 25, 45, 65, 86, 107, 128,
-                      149, 170, 191, 212, 223, 234, 250};
-int ptr;
-int mode;
+#include <avr/io.h>
+#include <util/delay.h>
+#include <xc.h>
 
-void PWM_init() {
-/*
- * INIT PWM
- * f_PWM = f_CLK/[N*(1+TOP)]
- * (f_PWM, f_CLK, TOP) = (62500, 16000000, 255) => N = 1
- * (COM1A0, COM1A1) = (0, 1) => OC1A ~ PB1
- * (WGM10, WGM11) = (1, 0) => Fast PWM, 8-bit
- * (WGM12, WGM13) = (1, 0) => Fast PWM, 8-bit
- * (CS10, CS11, CS12) = (0, 0, 1) => N = 1
- */
-    DDRD = (0<<PIND1) | (0<<PIND2) | (0<<PIND6) | (0<<PIND7);
-    PORTD |= (1<<PIND1) | (1<<PIND2) | (1<<PIND6) | (1<<PIND7);
-    TCCR1A = (1<<WGM10) | (1<<COM1A1);
-    TCCR1B = (1<<WGM12) | (1<<CS10);
-    DC_VALUE = 128;
-    ptr = 6;
-    mode = 0;
-    OCR1A = DC_VALUE;
-}
-
-void update_PWM_Duty_Cycle(int increment) {
-  /*
-   * if 5 < DC_VALUE < 250 then increment
-   * else no increment
-   * if increment = 0 then decrease
-   * else increase
-  */
-    if(increment)
-        if(DC_VALUE == 250)
-            break;
-        else DC_VALUE = OCR1A_table(++ptr);
-    else
-        if(DC_VALUE == 5)
-            break;
-        else DC_VALUE = OCR1A_table(++ptr);
-    OCR1A = DC_VALUE;
-}
-
-void update_Mode (int mode) {
-    if(mode) mode = 0;
-    else mode = 1;
-}
 
 int main(void) {
-    PWM_init();
-    while(1) {
+	
+	unsigned int OCR1A_table[14] = {5, 25, 45, 65, 86, 107, 128,
+	149, 170, 191, 212, 223, 234, 250};
+	unsigned char ptr = 6;
+	unsigned char mode = 0;
+	DDRB = (1<<PINB1);
+	DDRC = (0<<PINC1) | (0<<PINC0);
+	DDRD = (0<<PIND1) | (0<<PIND2) | (0<<PIND6) | (0<<PIND7);
+	PORTD = (1<<PIND1) | (1<<PIND2) | (1<<PIND6) | (1<<PIND7);
 
-        if(!(PIND & (1 << PIND6)))
-                update_Mode(0);
-        
-        if(!(PIND & (1 << PIND7)))
-                update_Mode(1);
-        
-        if(mode) {
-        }
-        else {
-            if(!(PIND & (1 << PIND1)))
-                update_PWM_Duty_Cycle(1);
+	TCCR1A = (1<<WGM10) | (1<<COM1A1);
+	TCCR1B = (1<<WGM12) | (1<<CS10);
 
-            if(!(PIND & (1 << PIND2)))
-                update_PWM_Duty_Cycle(0);
-        }
-    }
-    return 0;
+	OCR1AL = OCR1A_table[ptr];
+
+	ADMUX = 0b01000001;
+	ADCSRA = 0b10000111;
+	//  ADCSRB = 0x00; => free running => delay = 25 + 13k cycles, k>=0
+
+	while(1) {
+		// start conversion
+		ADCSRA|=(1<<ADSC);
+		while(ADCSRA & (1<<ADSC));
+		
+		// mode 1
+		if (!(PIND & (1<<PIND6))) {
+			while(!(PIND & (1<<PIND6))) _delay_ms(10);
+			ADMUX = 0b01000001;
+			mode = 0;
+			ADCSRA |= (1<<ADSC);
+			while(ADCSRA & (1<<ADSC));
+		}
+		
+		// mode 2
+		if (!(PIND & (1<<PIND7))) {
+			while(!(PIND & (1<<PIND7))) _delay_ms(10);
+			ADMUX = 0b01000000;
+			mode = 1;
+			ADCSRA |= (1<<ADSC);
+			while(ADCSRA & (1<<ADSC));
+		}
+		
+		if(mode){
+			OCR1AL = ADC/5.0;
+		}
+		else {
+			// PD1 => DC +
+			if (!(PIND & (1<<PIND1))) {
+				while(!(PIND & (1<<PIND1))) _delay_ms(10);
+				if(ptr != 13) OCR1AL = OCR1A_table[++ptr];
+			}
+			
+			// PD2 => PD -
+			if (!(PIND & (1<<PIND2))) {
+				while(!(PIND & (1<<PIND2))) _delay_ms(10);
+				if(ptr != 0) OCR1AL = OCR1A_table[--ptr];
+			}
+		}
+	}
+	return 0;
 }
